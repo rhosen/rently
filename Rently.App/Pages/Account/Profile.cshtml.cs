ca96ec445e889479d8e79befc3c70e13536aa398
Rubel Hosen <rhosen@wcgclinical.com>
2025-07-10 17:19:55 +0600

    clean up

--- Rently.App/Pages/Account/Profile.cshtml.cs
+++ Rently.App/Pages/Account/Profile.cshtml.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Rently.App.Configs;
